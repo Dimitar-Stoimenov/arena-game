a7724e8b24c9f07ca8537d7fe65d9bcd2455b8ef
Dimitar Stoimenov <dstoimen0@gmail.com>
2023-08-06 02:02:50 +0300

    added secondary effects list if the main is not enough

--- src/components/EffectsList/EffectsList.js
+++ src/components/EffectsList/EffectsList.js
@@ -2,10 +2,25 @@ import { Effect } from 'components';
 import styles from './styles.module.css';
 
 export const EffectsList = ({ effects, backgroundColor }) => {
-    return <div
-        style={{ backgroundColor: backgroundColor }}
-        className={styles.effects}
-    >
-        {effects.map((e, i) => <Effect effect={e} key={i} />)}
-    </div>;
+    const mainEffectsList = effects.slice(0, 4);
+    const secondaryEffectsList = effects.slice(4);
+    return (
+        <>
+            <div
+                style={{ backgroundColor: backgroundColor }}
+                className={styles.effects}
+            >
+                {mainEffectsList.map((e, i) => <Effect effect={e} key={i} />)}
+            </div>
+            <div
+                style={{
+                    backgroundColor: backgroundColor,
+                    opacity: effects.length > 4 ? 1 : 0
+                }}
+                className={styles.effectsOutside}
+            >
+                {secondaryEffectsList.map((e, i) => <Effect effect={e} key={i} />)}
+            </div>
+        </>
+    );
 };
